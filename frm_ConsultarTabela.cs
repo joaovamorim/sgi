3b5cba2b03ce755115b53eb07452795e29c38242
João Victor Amorim <joao.vamorim@outlook.com.br>
2024-05-24 19:55:12 -0300

    Criação Form Consultar Tabela - Inicial

---
+++ frm_ConsultarTabela.cs
@@ -0,0 +1,20 @@
+﻿namespace SAA
+{
+    public partial class frm_ConsultarTabela : Form
+    {
+        public frm_ConsultarTabela()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_ConsultarTabela_Load(object sender, EventArgs e)
+        {
+            this.Dock = DockStyle.Fill;
+        }
+
+        private void btn_RecarregarTabela_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
