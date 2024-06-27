bed4602dade75f6fe25a713793b796ad86d536c2
JOÃO VICTOR AMORIM <joao.vamorim@outlook.com.br>
2024-06-27 14:20:14 -0300

    ciracao frmCustomersDb da do botao consultar

---
+++ frm/ConsultDatabase/frmCustomersDb/frmCustomersDb.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace SGI.frm.ConsultDatabase.frmCustomersDb
+{
+    partial class frmCustomersDb
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCustomersDb));
+            toolStrip1 = new ToolStrip();
+            refreshToolStripButton = new ToolStripButton();
+            helpToolStripButton = new ToolStripButton();
+            toolStripButton1 = new ToolStripButton();
+            toolStripTextBox1 = new ToolStripTextBox();
+            grid_CadastroAluno = new DataGridView();
+            matricula = new DataGridViewTextBoxColumn();
+            nome = new DataGridViewTextBoxColumn();
+            dataNascimento = new DataGridViewTextBoxColumn();
+            cpf = new DataGridViewTextBoxColumn();
+            telefone = new DataGridViewTextBoxColumn();
+            email = new DataGridViewTextBoxColumn();
+            _status = new DataGridViewTextBoxColumn();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)grid_CadastroAluno).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.GripStyle = ToolStripGripStyle.Hidden;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { refreshToolStripButton, helpToolStripButton, toolStripButton1, toolStripTextBox1 });
+            toolStrip1.Location = new Point(0, 425);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.RenderMode = ToolStripRenderMode.Professional;
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // refreshToolStripButton
+            // 
+            refreshToolStripButton.Alignment = ToolStripItemAlignment.Right;
+            refreshToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            refreshToolStripButton.Image = (Image)resources.GetObject("refreshToolStripButton.Image");
+            refreshToolStripButton.ImageTransparentColor = Color.Magenta;
+            refreshToolStripButton.Name = "refreshToolStripButton";
+            refreshToolStripButton.Size = new Size(23, 22);
+            refreshToolStripButton.Tag = "";
+            refreshToolStripButton.Text = "Refresh";
+            // 
+            // helpToolStripButton
+            // 
+            helpToolStripButton.Alignment = ToolStripItemAlignment.Right;
+            helpToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            helpToolStripButton.Image = (Image)resources.GetObject("helpToolStripButton.Image");
+            helpToolStripButton.ImageTransparentColor = Color.Magenta;
+            helpToolStripButton.Name = "helpToolStripButton";
+            helpToolStripButton.Size = new Size(23, 22);
+            helpToolStripButton.Text = "Help";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.BackColor = SystemColors.ControlLight;
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(46, 22);
+            toolStripButton1.Text = "Search";
+            // 
+            // toolStripTextBox1
+            // 
+            toolStripTextBox1.BackColor = Color.White;
+            toolStripTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            toolStripTextBox1.Name = "toolStripTextBox1";
+            toolStripTextBox1.Size = new Size(100, 25);
+            toolStripTextBox1.Tag = "Search";
+            // 
+            // grid_CadastroAluno
+            // 
+            grid_CadastroAluno.AllowUserToAddRows = false;
+            grid_CadastroAluno.AllowUserToDeleteRows = false;
+            grid_CadastroAluno.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            grid_CadastroAluno.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grid_CadastroAluno.Columns.AddRange(new DataGridViewColumn[] { matricula, nome, dataNascimento, cpf, telefone, email, _status });
+            grid_CadastroAluno.Dock = DockStyle.Fill;
+            grid_CadastroAluno.Location = new Point(0, 0);
+            grid_CadastroAluno.MultiSelect = false;
+            grid_CadastroAluno.Name = "grid_CadastroAluno";
+            grid_CadastroAluno.ReadOnly = true;
+            grid_CadastroAluno.RowTemplate.Height = 25;
+            grid_CadastroAluno.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            grid_CadastroAluno.Size = new Size(800, 425);
+            grid_CadastroAluno.TabIndex = 20;
+            // 
+            // matricula
+            // 
+            matricula.HeaderText = "MATRÍCULA";
+            matricula.Name = "matricula";
+            matricula.ReadOnly = true;
+            // 
+            // nome
+            // 
+            nome.HeaderText = "NOME";
+            nome.Name = "nome";
+            nome.ReadOnly = true;
+            // 
+            // dataNascimento
+            // 
+            dataNascimento.HeaderText = "DATA NASCIMENTO";
+            dataNascimento.Name = "dataNascimento";
+            dataNascimento.ReadOnly = true;
+            // 
+            // cpf
+            // 
+            cpf.HeaderText = "CPF";
+            cpf.Name = "cpf";
+            cpf.ReadOnly = true;
+            // 
+            // telefone
+            // 
+            telefone.HeaderText = "TELEFONE";
+            telefone.Name = "telefone";
+            telefone.ReadOnly = true;
+            // 
+            // email
+            // 
+            email.HeaderText = "E-MAIL";
+            email.Name = "email";
+            email.ReadOnly = true;
+            // 
+            // _status
+            // 
+            _status.HeaderText = "STATUS";
+            _status.Name = "_status";
+            _status.ReadOnly = true;
+            // 
+            // frmCustomersDb
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(grid_CadastroAluno);
+            Controls.Add(toolStrip1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmCustomersDb";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SGI | ";
+            WindowState = FormWindowState.Maximized;
+            Load += frmCustomersDb_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)grid_CadastroAluno).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton refreshToolStripButton;
+        private ToolStripButton helpToolStripButton;
+        private DataGridView grid_CadastroAluno;
+        private DataGridViewTextBoxColumn matricula;
+        private DataGridViewTextBoxColumn nome;
+        private DataGridViewTextBoxColumn dataNascimento;
+        private DataGridViewTextBoxColumn cpf;
+        private DataGridViewTextBoxColumn telefone;
+        private DataGridViewTextBoxColumn email;
+        private DataGridViewTextBoxColumn _status;
+        private ToolStripButton toolStripButton1;
+        private ToolStripTextBox toolStripTextBox1;
+    }
+}
---
+++ frm/ConsultDatabase/frmCustomersDb/frmCustomersDb.cs
@@ -0,0 +1,15 @@
+﻿namespace SGI.frm.ConsultDatabase.frmCustomersDb
+{
+    public partial class frmCustomersDb : Form
+    {
+        public frmCustomersDb()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomersDb_Load(object sender, EventArgs e)
+        {
+            this.Dock = DockStyle.Fill;
+        }
+    }
+}
