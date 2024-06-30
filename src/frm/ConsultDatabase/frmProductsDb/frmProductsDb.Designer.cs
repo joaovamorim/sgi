20ff9700ba83b40e2b378a1bae209a6147ed533b
JOÃO VICTOR AMORIM <joao.vamorim@outlook.com.br>
2024-06-30 14:15:05 -0300

    Organizacao do condigo fonte nos diretorios

---
+++ src/frm/ConsultDatabase/frmProductsDb/frmProductsDb.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace SGI.frm.ConsultDatabase.frmProductsDb
+{
+    partial class frmProductsDb
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProductsDb));
+            toolStrip1 = new ToolStrip();
+            helpToolStripButton = new ToolStripButton();
+            refreshToolStripButton = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            searchToolStripButton = new ToolStripButton();
+            toolStripTextBox1 = new ToolStripTextBox();
+            toolStripSeparator4 = new ToolStripSeparator();
+            toolStripLabel1 = new ToolStripLabel();
+            gridProductsDb = new DataGridView();
+            matricula = new DataGridViewTextBoxColumn();
+            nome = new DataGridViewTextBoxColumn();
+            dataNascimento = new DataGridViewTextBoxColumn();
+            cpf = new DataGridViewTextBoxColumn();
+            telefone = new DataGridViewTextBoxColumn();
+            email = new DataGridViewTextBoxColumn();
+            _status = new DataGridViewTextBoxColumn();
+            timer1 = new System.Windows.Forms.Timer(components);
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)gridProductsDb).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { helpToolStripButton, refreshToolStripButton, toolStripSeparator1, searchToolStripButton, toolStripTextBox1, toolStripSeparator4, toolStripLabel1 });
+            toolStrip1.Location = new Point(0, 425);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.RenderMode = ToolStripRenderMode.Professional;
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // helpToolStripButton
+            // 
+            helpToolStripButton.Alignment = ToolStripItemAlignment.Right;
+            helpToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            helpToolStripButton.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            helpToolStripButton.Image = (Image)resources.GetObject("helpToolStripButton.Image");
+            helpToolStripButton.ImageTransparentColor = Color.Magenta;
+            helpToolStripButton.Name = "helpToolStripButton";
+            helpToolStripButton.Size = new Size(23, 22);
+            helpToolStripButton.Text = "Help";
+            // 
+            // refreshToolStripButton
+            // 
+            refreshToolStripButton.Alignment = ToolStripItemAlignment.Right;
+            refreshToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            refreshToolStripButton.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            refreshToolStripButton.Image = (Image)resources.GetObject("refreshToolStripButton.Image");
+            refreshToolStripButton.ImageTransparentColor = Color.Magenta;
+            refreshToolStripButton.Name = "refreshToolStripButton";
+            refreshToolStripButton.Size = new Size(23, 22);
+            refreshToolStripButton.Tag = "";
+            refreshToolStripButton.Text = "Refresh";
+            refreshToolStripButton.Click += refreshToolStripButton_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 25);
+            // 
+            // searchToolStripButton
+            // 
+            searchToolStripButton.BackColor = SystemColors.ControlLight;
+            searchToolStripButton.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            searchToolStripButton.ForeColor = SystemColors.ActiveCaptionText;
+            searchToolStripButton.Image = (Image)resources.GetObject("searchToolStripButton.Image");
+            searchToolStripButton.ImageTransparentColor = Color.Magenta;
+            searchToolStripButton.Name = "searchToolStripButton";
+            searchToolStripButton.Size = new Size(67, 22);
+            searchToolStripButton.Text = "Search";
+            searchToolStripButton.Click += searchToolStripButton_Click;
+            // 
+            // toolStripTextBox1
+            // 
+            toolStripTextBox1.AutoSize = false;
+            toolStripTextBox1.BackColor = Color.White;
+            toolStripTextBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            toolStripTextBox1.Name = "toolStripTextBox1";
+            toolStripTextBox1.Size = new Size(100, 25);
+            toolStripTextBox1.Tag = "Search";
+            toolStripTextBox1.KeyDown += toolStripTextBox1_KeyDown;
+            // 
+            // toolStripSeparator4
+            // 
+            toolStripSeparator4.Name = "toolStripSeparator4";
+            toolStripSeparator4.Size = new Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            toolStripLabel1.BackColor = SystemColors.Control;
+            toolStripLabel1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            toolStripLabel1.Name = "toolStripLabel1";
+            toolStripLabel1.Size = new Size(118, 22);
+            toolStripLabel1.Text = "Products Database";
+            // 
+            // gridProductsDb
+            // 
+            gridProductsDb.AllowUserToAddRows = false;
+            gridProductsDb.AllowUserToDeleteRows = false;
+            gridProductsDb.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            gridProductsDb.BackgroundColor = SystemColors.Control;
+            gridProductsDb.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridProductsDb.Columns.AddRange(new DataGridViewColumn[] { matricula, nome, dataNascimento, cpf, telefone, email, _status });
+            gridProductsDb.Dock = DockStyle.Fill;
+            gridProductsDb.GridColor = SystemColors.Control;
+            gridProductsDb.Location = new Point(0, 0);
+            gridProductsDb.MultiSelect = false;
+            gridProductsDb.Name = "gridProductsDb";
+            gridProductsDb.ReadOnly = true;
+            gridProductsDb.RowTemplate.Height = 25;
+            gridProductsDb.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            gridProductsDb.Size = new Size(800, 425);
+            gridProductsDb.TabIndex = 20;
+            // 
+            // matricula
+            // 
+            matricula.HeaderText = "MATRÍCULA";
+            matricula.Name = "matricula";
+            matricula.ReadOnly = true;
+            // 
+            // nome
+            // 
+            nome.HeaderText = "NOME";
+            nome.Name = "nome";
+            nome.ReadOnly = true;
+            // 
+            // dataNascimento
+            // 
+            dataNascimento.HeaderText = "DATA NASCIMENTO";
+            dataNascimento.Name = "dataNascimento";
+            dataNascimento.ReadOnly = true;
+            // 
+            // cpf
+            // 
+            cpf.HeaderText = "CPF";
+            cpf.Name = "cpf";
+            cpf.ReadOnly = true;
+            // 
+            // telefone
+            // 
+            telefone.HeaderText = "TELEFONE";
+            telefone.Name = "telefone";
+            telefone.ReadOnly = true;
+            // 
+            // email
+            // 
+            email.HeaderText = "E-MAIL";
+            email.Name = "email";
+            email.ReadOnly = true;
+            // 
+            // _status
+            // 
+            _status.HeaderText = "STATUS";
+            _status.Name = "_status";
+            _status.ReadOnly = true;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 500;
+            timer1.Tick += timer1_Tick;
+            // 
+            // frmProductsDb
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(gridProductsDb);
+            Controls.Add(toolStrip1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmProductsDb";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Products Database";
+            WindowState = FormWindowState.Maximized;
+            Load += frmProductsDb_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)gridProductsDb).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton refreshToolStripButton;
+        private ToolStripButton helpToolStripButton;
+        private DataGridView gridProductsDb;
+        private DataGridViewTextBoxColumn matricula;
+        private DataGridViewTextBoxColumn nome;
+        private DataGridViewTextBoxColumn dataNascimento;
+        private DataGridViewTextBoxColumn cpf;
+        private DataGridViewTextBoxColumn telefone;
+        private DataGridViewTextBoxColumn email;
+        private DataGridViewTextBoxColumn _status;
+        private ToolStripButton searchToolStripButton;
+        private ToolStripTextBox toolStripTextBox1;
+        private ToolStripSeparator toolStripSeparator4;
+        private ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private ToolStripSeparator toolStripSeparator1;
+    }
+}
